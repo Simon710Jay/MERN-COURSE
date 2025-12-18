a2cdfccec12bdea52c63a64d5b3bd70adfbaea19
Simon Ejeh <simonjay710@gmail.com>
2025-12-18 19:39:38 +0100

    Download files

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";  
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+
+import productRouters from "./routes/product.route.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRouters)
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("server started at http://localhost:" + PORT);
+});
+
+
+
